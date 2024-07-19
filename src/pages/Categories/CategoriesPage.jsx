ba7c8564b5698d639059adf7fa9c42392853733d
Slava Bezgachev <bezgachev@gmail.com>
2024-07-19 15:20:50 +0500

    Added filter

--- src/pages/Categories/CategoriesPage.jsx
+++ src/pages/Categories/CategoriesPage.jsx
@@ -1,19 +1,78 @@
 import { Link } from "react-router-dom";
 
+const categories = [
+  {
+    id: 1,
+    name: "Dry & Wet Food",
+  },
+  {
+    id: 2,
+    name: "Litter Boxes & Litter Trays",
+  },
+  {
+    id: 3,
+    name: "Baskets & Beds",
+  },
+  {
+    id: 4,
+    name: "Scratching Posts",
+  },
+  {
+    id: 5,
+    name: "Toys",
+  },
+  {
+    id: 6,
+    name: "Collars & Leashes",
+  },
+  {
+    id: 7,
+    name: "Carriers & Transport",
+  },
+  {
+    id: 8,
+    name: "Bowls & Feeders",
+  },
+  {
+    id: 9,
+    name: "Grooming",
+  },
+  {
+    id: 10,
+    name: "Health & Hygiene",
+  },
+  {
+    id: 11,
+    name: "Accessories",
+  },
+  {
+    id: 12,
+    name: "Training",
+  },
+  {
+    id: 13,
+    name: "Gifts",
+  },
+  {
+    id: 14,
+    name: "New Arrivals",
+  },
+  {
+    id: 15,
+    name: "Sale",
+  },
+];
+
 export default function CategoriesPage() {
   return (
     <div>
       <p>Categories Page</p>
       <ul>
-        <li>
-          <Link to="/categories/1">Category 1</Link>
-        </li>
-        <li>
-          <Link to="/categories/2">Category 2</Link>
-        </li>
-        <li>
-          <Link to="/categories/3">Category 3</Link>
-        </li>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <Link to={`/categories/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
