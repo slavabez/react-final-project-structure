bf20247e79dae699544af5dcb4f033fc495e61d7
Slava Bezgachev <bezgachev@gmail.com>
2024-07-26 17:10:07 +0500

    category page from axios

--- src/pages/Categories/CategoriesPage.jsx
+++ src/pages/Categories/CategoriesPage.jsx
@@ -1,67 +1,6 @@
 import { Link } from "react-router-dom";
 
-const categories = [
-  {
-    id: 1,
-    name: "Dry & Wet Food",
-  },
-  {
-    id: 2,
-    name: "Litter Boxes & Litter Trays",
-  },
-  {
-    id: 3,
-    name: "Baskets & Beds",
-  },
-  {
-    id: 4,
-    name: "Scratching Posts",
-  },
-  {
-    id: 5,
-    name: "Toys",
-  },
-  {
-    id: 6,
-    name: "Collars & Leashes",
-  },
-  {
-    id: 7,
-    name: "Carriers & Transport",
-  },
-  {
-    id: 8,
-    name: "Bowls & Feeders",
-  },
-  {
-    id: 9,
-    name: "Grooming",
-  },
-  {
-    id: 10,
-    name: "Health & Hygiene",
-  },
-  {
-    id: 11,
-    name: "Accessories",
-  },
-  {
-    id: 12,
-    name: "Training",
-  },
-  {
-    id: 13,
-    name: "Gifts",
-  },
-  {
-    id: 14,
-    name: "New Arrivals",
-  },
-  {
-    id: 15,
-    name: "Sale",
-  },
-];
+const categories = [];
 
 export default function CategoriesPage() {
   return (
--- src/pages/Products/ByCategory/ProductsByCategoryPage.jsx
+++ src/pages/Products/ByCategory/ProductsByCategoryPage.jsx
@@ -1,12 +1,64 @@
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function ProductsByCategoryPage() {
   const { categoryId } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [categoryName, setCategoryName] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch products by category ID
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:3333/categories/${categoryId}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setProducts(response.data.data);
+          setCategoryName(response.data.category.title);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError("An error occurred fetching data. Please try again later.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [categoryId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      <p>Products By Category Page</p>
-      <p>Category ID: {categoryId}</p>
+      <h1>{categoryName}</h1>
+      {error && (
+        <p
+          style={{
+            color: "red",
+            backgroundColor: "pink",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
+        >
+          {error}
+        </p>
+      )}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+              <p>Price: {product.price}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
